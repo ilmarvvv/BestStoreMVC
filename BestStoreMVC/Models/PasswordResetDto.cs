0e53b098fee1169409d63ff9b46b7a707db198af
AngryCarrot <ilmarvvv@gmail.com>
2025-01-25 18:22:25 -0500

    Add project files.

---
+++ BestStoreMVC/Models/PasswordResetDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class PasswordResetDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Password { get; set; } = "";
+
+        [Required(ErrorMessage = "The Confirm Password field is required")]
+        [Compare("Password", ErrorMessage = "Confirm Password field is required")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
